47d2ee4cfb7fb7d677b036772217f8fcdfa7dfe7
Sergey <zavulon31990@gmail.com>
2022-02-03 11:32:11 +0300

    feat: add components and types directory

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './footer.css';
+
+export const Footer: React.FC = () => {
+  return <div className = 'footer'></div>
+};
---
+++ src/components/header/header.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import './header.css';
+export const Header: React.FC = () => {
+  return <div className = 'header'></div>
+};
