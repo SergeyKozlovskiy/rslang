fe1bda3c3c7bc50364f188c099f53d4566983749
Sergey <zavulon31990@gmail.com>
2022-02-03 11:46:25 +0300

    feat: remove test files

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -2,5 +2,7 @@ import React from 'react';
 import './footer.css';
 
 export const Footer: React.FC = () => {
-  return <div className = 'footer'></div>
+  return <div className = 'footer'>
+    <h1>Footer</h1>
+  </div>
 };
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './header.css';
 export const Header: React.FC = () => {
-  return <div className = 'header'></div>
+  return <div className = 'header'>
+    <h1>Header</h1>
+  </div>
 };
