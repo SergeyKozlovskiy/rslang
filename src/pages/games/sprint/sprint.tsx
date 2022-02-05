4d3ebce392d17b3bbe995cfca3f65d93360d843f
Sergey <zavulon31990@gmail.com>
2022-02-05 16:40:18 +0300

    feat: create a prototype game sprint

--- src/pages/games/sprint/sprint.tsx
+++ src/pages/games/sprint/sprint.tsx
@@ -4,48 +4,100 @@ import { timer } from '../../../functions/timer';
 import _ from 'lodash';
 import { getWords } from '../../../requests/getWords';
 import './sprint.css';
+import { MagicNumbers, Text } from '../../../types/enums';
+import { useState } from 'react';
 
 type Word = {
-    id: "string",
+    id: string,
     group: number,
     page: number,
-    word: "string",
-    image: "string",
-    audio: "string",
-    audioMeaning: "string",
-    audioExample: "string",
-    textMeaning: "string",
-    textExample: "string",
-    transcription: "string",
-    wordTranslate: "string",
-    textMeaningTranslate: "string",
-    textExampleTranslate: "string"
+    word: string,
+    image: string,
+    audio: string,
+    audioMeaning: string,
+    audioExample: string,
+    textMeaning: string,
+    textExample: string,
+    transcription: string,
+    wordTranslate: string,
+    textMeaningTranslate: string,
+    textExampleTranslate: string
 }
 
 type Answer = {
-  word: "string",
-  translate: "string",
+  word: string,
+  translate: string,
 }
 
+type Question = {
+  word: string,
+  translate: string, 
+  answer: boolean
+}
+
+let dataWords : Word[];
+let rightAnswers : Answer[] = [];
+let numQuestion = 0;
 
 export const Sprint: React.FC = () => {
-  let dataWords : Word[];
-  let rightAnswers : Answer[] = [];
-  let numQuestion = 0;
+  const [questionData, setQuestion] = useState<Question>({
+    word: '',
+    translate: '', 
+    answer:  true
+  });
+
+
+  async function getData () {
+    try {
+      const randomGroup = _.random(0, 5);
+      const randomPage = _.random(0, 29);
+      const result = await getWords(randomGroup, randomPage).then((value) => value);
+      dataWords = result;
+      createArrAnswers();
+    }
+    catch(error){
+      console.log(error);
+    }
+  }
+
+  const responseСheck = (answer: boolean) => {
+    const questionCard = document.querySelector('.question-card') as HTMLElement;
+    if(questionData.answer === answer && questionCard){
+      console.log('правильно');
+      questionCard.classList.add('right-answer');
+      setTimeout(() => {
+        questionCard.classList.remove('right-answer');
+      }, 400)
+    }else{
+      console.log('не правильно')
+      questionCard.classList.add('wrong-answer');
+      setTimeout(() => {
+        questionCard.classList.remove('wrong-answer');
+      }, 400)
+    }
+    console.log(numQuestion);
+    createQuestion();
+  }
 
   const createQuestion = () => {
-    const randomNum = _.random(0, 1);
-    const question = {
-      word : rightAnswers[numQuestion],
-      translate : rightAnswers[numQuestion + randomNum],
-      answer : [randomNum === 0 ? true : false]
+    if(numQuestion < MagicNumbers.MAX_NUM_OF_QUESTIONS) {
+      const randomNum = _.random(0, 1);
+      const question = {
+        word : rightAnswers[numQuestion].word,
+        translate : rightAnswers[numQuestion + randomNum].translate,
+        answer : randomNum === 0 ? true : false
+      }
+      numQuestion++;
+      setQuestion(question);
+    }else if(numQuestion === MagicNumbers.MAX_NUM_OF_QUESTIONS){
+      dataWords = [];
+      rightAnswers = [];
+      numQuestion = 0;
+      getData();
     }
-    return question;
   }
 
   const createArrAnswers = () => {
-    console.log(dataWords);
-
     dataWords.forEach((elem, i) => {
       const answer = {
         word: elem.word,
@@ -53,49 +105,52 @@ export const Sprint: React.FC = () => {
       }
       rightAnswers.push(answer)
     })
-
     console.log(rightAnswers);
+    createQuestion();
   }
 
   const start = () => {
     const popUp = document.querySelector('.sprint-popup') as HTMLElement;
+    const questionСard = document.querySelector('.question-card') as HTMLElement;
+    const timerElem = document.getElementById('timer') as HTMLElement;
     popUp?.classList.add('hide-popup');
+    questionСard?.classList.remove('hide-popup');
+    timerElem?.classList.remove('hide-popup');
     timer();
-    async function getData () {
-      try {
-        const randomGroup = _.random(0, 5);
-        const randomPage = _.random(0, 29);
-        const result = await getWords(randomGroup, randomPage).then((value) => value);
-        dataWords = result;
-        createArrAnswers();
-      }
-      catch(error){
-        console.log(error);
-      }
-    }
     getData();
-
-
   }
+
+
+
 
   return <div className="sprint-wrapper">
     <div className="sprint-settings">
-      <button><img src={fullScreen} alt="fullScreen"/></button>
+      <button><img src={fullScreen} alt={fullScreen}/></button>
       <DropdownButton className='dropdown-level' title="Уровень ">
-        <Dropdown.Item href="#/action-1">Уровень 1</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Уровень 2</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Уровень 3</Dropdown.Item>
-        <Dropdown.Item href="#/action-1">Уровень 4</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Уровень 5</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Уровень 5</Dropdown.Item>
+        <Dropdown.Item href="">{Text.DropdownText} 1</Dropdown.Item>
+        <Dropdown.Item href="">{Text.DropdownText} 2</Dropdown.Item>
+        <Dropdown.Item href="">{Text.DropdownText} 3</Dropdown.Item>
+        <Dropdown.Item href="">{Text.DropdownText} 4</Dropdown.Item>
+        <Dropdown.Item href="">{Text.DropdownText} 5</Dropdown.Item>
+        <Dropdown.Item href="">{Text.DropdownText} 6</Dropdown.Item>
       </DropdownButton>
       <CloseButton />
     </div>
+
     <div className="sprint-popup">
-      <h3>Спринт</h3>
-      <p>Выберите соответсвует ли перевод предложенному слову</p>
-      <Button onClick={start} className='sprint-popup_btn' variant="secondary">Начать</Button>
+      <h3>{Text.HeaderSprintPopUp}</h3>
+      <p>{Text.SubtitleSprintPopUp}</p>
+      <Button onClick={start} className='sprint-popup_btn' variant="success">{Text.StartSprintButton}</Button>
     </div>
-    <div id="timer"></div>
+
+    <div id="timer" className='hide-popup'></div>
+
+    <div className="question-card hide-popup">
+      <p className="question-card_word">{questionData.word}</p>
+      <p className="question-card_translate">{questionData.translate}</p>
+      <Button className='question-card_btn' onClick={() => {responseСheck(false)}} variant="danger">{Text.WrongAnswerSprintButton}</Button>
+      <Button className='question-card_btn' onClick={() => {responseСheck(true)}} variant="success">{Text.RightAnswerSprintButton}</Button>
+    </div>
+
   </div>
 };
--- src/types/enums.ts
+++ src/types/enums.ts
@@ -1,8 +1,17 @@
 export enum Text {
-
+  DropdownText = 'Уровень',
+  HeaderSprintPopUp = 'Спринт',
+  SubtitleSprintPopUp = 'Выберите соответсвует ли перевод предложенному слову',
+  StartSprintButton = 'Начать',
+  RightAnswerSprintButton = 'Верно',
+  WrongAnswerSprintButton = 'Не верно'
 }
 
 export enum API {
   URL = 'http://localhost:5000/',
   Words = 'words'
+}
+
+export enum MagicNumbers {
+  MAX_NUM_OF_QUESTIONS = 20
 }
