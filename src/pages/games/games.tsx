237dca84897536bd304390a7f3b1431850e8c18a
Sergey <zavulon31990@gmail.com>
2022-02-20 12:39:05 +0300

    fix: fix the formula for calculating the correct answers

--- src/pages/games/games.tsx
+++ src/pages/games/games.tsx
@@ -1,11 +1,12 @@
 import { Link } from 'react-router-dom';
 import './games.css';
+
 export const Games: React.FC = () => {
   return <div className="games-wrapper">
         <nav className="nav-game" >
           <ul>
-            <li><Link className="nav-game_link" to="/sprint">Спринт</Link></li>
-            <li><Link className="nav-game_link" to="/audioCall">Аудиовызов</Link></li>
+            <li className='nav-game__li'><Link className="nav-game_link" to="/sprint"></Link></li>
+            <li className='nav-game__li'><Link className="nav-game_link" to="/audioCall"></Link></li>
           </ul>
         </nav>
   </div>
