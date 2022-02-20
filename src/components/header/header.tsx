b8736f71b6eee61b34167145176eec9a2cb8625d
”Stellarator85” <stellarator85@gmail.com>
2022-02-20 19:04:33 +0200

    feat: adjust menu-panel

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -13,6 +13,7 @@ import { getUserInfo } from '../../localStorage/getUserInfo';
 import { IRespSignIn } from '../../types/types';
 import HomeIcon from '../../assets/svg/menu-home.svg';
 import LoginIcon from '../../assets/svg/menu-login.svg';
+import TextbookIcon from '../../assets/svg/menu-textbook.svg';
 import VocabularyIcon from '../../assets/svg/menu-vocabulary.svg';
 import GamesIcon from '../../assets/svg/menu-games.svg';
 import StatisticsIcon from '../../assets/svg/menu-statistics.svg';
@@ -21,6 +22,7 @@ import AboutIcon from '../../assets/svg/menu-about.svg';
 export const Header: React.FC = () => {
   const showMenu = () => {
     (document.getElementById("menu_list") as HTMLFormElement).classList.toggle("menu-active");
+    (document.querySelector(".app-menu-layer") as HTMLFormElement).classList.toggle("app-menu-layer-active"); 
     hideMenu();
   };
 
@@ -29,7 +31,10 @@ export const Header: React.FC = () => {
       if ((event.target !== document.getElementById("menu_list") as HTMLFormElement) &&
         (event.target !== document.querySelector("Button") as HTMLFormElement) &&
         (document.getElementById("menu_list") as HTMLFormElement).classList.contains("menu-active"))
-        return (document.getElementById("menu_list") as HTMLFormElement).classList.remove("menu-active");
+      {
+        (document.getElementById("menu_list") as HTMLFormElement).classList.remove("menu-active");
+        (document.querySelector(".app-menu-layer") as HTMLFormElement).classList.remove("app-menu-layer-active");
+    }
     })
   };
 
@@ -44,19 +49,19 @@ export const Header: React.FC = () => {
       <Button variant="outline-warning" className={Classes.menuButton} onClick={showMenu}></Button>
       <ul id="menu_list" className={Classes.menuList}>
         <CloseButton variant="white" className={Classes.menuListCloseButton} onClick={showMenu}></CloseButton>
-        <li className={Classes.menuListItem}><Link className={Classes.menuListItemOption} to="/" onClick={showMenu}><img alt="home" src={HomeIcon}></img>{Text.menuOptionStartPage}</Link></li>
+        <li className={Classes.menuListItem}><Link className={Classes.menuListItemOption} to="/" onClick={showMenu}><img alt="home" src={HomeIcon}></img>&nbsp;{Text.menuOptionStartPage}</Link></li>
         {
           state.IsLogin === false
             ?
-            <li className={Classes.menuListItem}><Link className={Classes.menuListItemOption} to="/authorization" onClick={showMenu}><img alt="Login" src={LoginIcon}></img>{Text.menuOptionLoginPage}</Link></li>
+            <li className={Classes.menuListItem}><Link className={Classes.menuListItemOption} to="/authorization" onClick={showMenu}><img alt="Login" src={LoginIcon}></img>&nbsp;{Text.menuOptionLoginPage}</Link></li>
             :
             ''
         }
-        <li className={Classes.menuListItem}><Link className={Classes.menuListItemOption} to="/book" onClick={showMenu}><img alt="Vocabulary" src={VocabularyIcon}></img>{Text.menuOptionElectronicBook}</Link></li>
-        <li className={Classes.menuListItem}><Link className={Classes.menuListItemOption} to="/dictionary" onClick={showMenu}>Словарь</Link></li>
-        <li className={Classes.menuListItem}><Link className={Classes.menuListItemOption} to="/games" onClick={showMenu}><img alt="Games" src={GamesIcon}></img>{Text.menuOptionMiniGames}</Link></li>
-        <li className={Classes.menuListItem}><Link className={Classes.menuListItemOption} to="/statistics" onClick={showMenu}><img alt="Statistics" src={StatisticsIcon}></img>{Text.menuOptionStatPage}</Link></li>
-        <li className={Classes.menuListItem}><Link className={Classes.menuListItemOption} to="/about" onClick={showMenu}><img alt="About" src={AboutIcon}></img>{Text.menuOptionAbout}</Link></li>
+        <li className={Classes.menuListItem}><Link className={Classes.menuListItemOption} to="/book" onClick={showMenu}><img alt="Vocabulary" src={TextbookIcon}></img>&nbsp;{Text.menuOptionTextbook}</Link></li>
+        <li className={Classes.menuListItem}><Link className={Classes.menuListItemOption} to="/dictionary" onClick={showMenu}><img alt="Vocabulary" src={VocabularyIcon}></img>&nbsp;{Text.menuOptionVocabulary}</Link></li>
+        <li className={Classes.menuListItem}><Link className={Classes.menuListItemOption} to="/games" onClick={showMenu}><img alt="Games" src={GamesIcon}></img>&nbsp;{Text.menuOptionMiniGames}</Link></li>
+        <li className={Classes.menuListItem}><Link className={Classes.menuListItemOption} to="/statistics" onClick={showMenu}><img alt="Statistics" src={StatisticsIcon}></img>&nbsp;&nbsp;{Text.menuOptionStatPage}</Link></li>
+        <li className={Classes.menuListItem}><Link className={Classes.menuListItemOption} to="/about" onClick={showMenu}><img alt="About" src={AboutIcon}></img>&nbsp;&nbsp;&nbsp;&nbsp;{Text.menuOptionAbout}</Link></li>
       </ul>
     </nav >
     <Link className={Classes.headerAppName} to="/">{Text.headerAppName}
